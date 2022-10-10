be3ba86b1150adfc66391fd3137b38a6fd345fbe
Isabela <isabela_cmoreira@hotmail.com>
2022-10-10 13:15:17 -0300

    feat: Context created for user

---
+++ src/contexts/UserContext.js
@@ -0,0 +1,17 @@
+
+
+import { createContext, useState } from "react";
+
+const UserContext  = createContext();
+
+const UserProvider = ({children}) => {
+    const [userData, setUserData] = useState(JSON.parse(localStorage.getItem("userData")));
+
+    return (
+        <UserContext.Provider value={{userData, setUserData}}>{children}</UserContext.Provider>
+    );
+};
+
+export {
+    UserContext, UserProvider
+}
