7c57f9e0e24bc419111633c593112e040c0cbcaf
Isabela <isabela_cmoreira@hotmail.com>
2022-10-10 13:18:59 -0300

    feat: created two more routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from "react";
 import {Route, Routes, BrowserRouter} from "react-router-dom";
-import {AuthProvider} from "./contexts/AuthContext";
+import {UserProvider} from "./contexts/UserContext";
+import { TokenProvider } from "./contexts/TokenContext";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import NailPolishes from "./pages/NailPolishes";
+import Shuffle from "./pages/Shuffle";
 
 
 function App() {
   return (
-    <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/login" element={<Login />} />
-        <Route path = "/signup" element={<SignUp/>}/>
-      </Routes>
-    </BrowserRouter>
-    </AuthProvider>
+    <UserProvider>
+      <TokenProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path = "/login" element={<Login />} />
+            <Route path = "/signup" element={<SignUp/>}/>
+            <Route path ="/nailpolishes" element ={<NailPolishes/>}/>
+            <Route path = "/shuffle" element= {<Shuffle/>}/>
+          </Routes>
+        </BrowserRouter>
+      </TokenProvider>
+    </UserProvider>
   );
 }
 
