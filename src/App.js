5f2e42bb66582de45a53afa0e36c2e444f5d35bd
Isabela <isabela_cmoreira@hotmail.com>
2023-04-04 16:57:49 -0300

    feat: decided to add all routes beside login and register to a home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import {Route, Routes, BrowserRouter} from "react-router-dom";
+import {Route, Routes, BrowserRouter, Navigate} from "react-router-dom";
+import { useContext } from "react";
 import {UserProvider} from "./contexts/UserContext";
+import { TokenContext } from "./contexts/TokenContext";
 import { TokenProvider } from "./contexts/TokenContext";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import NailPolishes from "./pages/NailPolishes";
-import Create from "./pages/Create";
-import Shuffle from "./pages/Shuffle";
+import NailPolishes from "./pages/Home/NailPolishes";
+import Create from "./pages/Home/Create";
+import Shuffle from "./pages/Home/Shuffle";
+import Home from "./pages/Home";
 
-
-function App() {
+export default function App() {
   return (
     <UserProvider>
       <TokenProvider>
@@ -17,9 +19,18 @@ function App() {
           <Routes>
             <Route path = "/login" element={<Login />} />
             <Route path = "/signup" element={<SignUp/>}/>
-            <Route path ="/nailpolishes" element ={<NailPolishes/>}/>
-            <Route path = "/create" element= {<Create/>}/>
-            <Route path="/random" element ={<Shuffle/>}/>
+            <Route path="/"
+                element={
+                  <ProtectedRouteGuard>
+                    <Home/>
+                  </ProtectedRouteGuard>
+                }
+                >
+              <Route path ="nailpolishes" element ={<NailPolishes/>}/>
+              <Route path = "create" element= {<Create/>}/>
+              <Route path="random" element ={<Shuffle/>}/>
+              <Route index path="*" element={<Navigate to="/" />} />
+            </Route>
           </Routes>
         </BrowserRouter>
       </TokenProvider>
@@ -27,4 +38,12 @@ function App() {
   );
 }
 
-export default App;
+function ProtectedRouteGuard({ children }) {
+  const { token } = useContext(TokenContext);
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  return <>{children}</>;
+}
