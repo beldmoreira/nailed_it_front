5ded9ac78db8580083e2efd98aa48f3297e6a611
Isabela <isabela_cmoreira@hotmail.com>
2023-04-15 23:47:56 -0300

    wip: changed back to this type of routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import {Route, Routes, BrowserRouter, Navigate} from "react-router-dom";
-import { useContext } from "react";
 import {UserProvider} from "./contexts/UserContext";
-import { TokenContext } from "./contexts/TokenContext";
 import { TokenProvider } from "./contexts/TokenContext";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import NailPolishes from "./pages/Home/NailPolishes";
-import Create from "./pages/Home/Create";
-import Shuffle from "./pages/Home/Shuffle";
-import Home from "./pages/Home";
+import NailPolishes from "./pages/NailPolishes";
+import Create from "./pages/Create";
+import Shuffle from "./pages/Shuffle";
 
 export default function App() {
   return (
@@ -19,31 +16,13 @@ export default function App() {
           <Routes>
             <Route path = "/login" element={<Login />} />
             <Route path = "/signup" element={<SignUp/>}/>
-            <Route path="/"
-                element={
-                  <ProtectedRouteGuard>
-                    <Home/>
-                  </ProtectedRouteGuard>
-                }
-                >
-              <Route path ="nailpolishes" element ={<NailPolishes/>}/>
-              <Route path = "create" element= {<Create/>}/>
-              <Route path="random" element ={<Shuffle/>}/>
-              <Route index path="*" element={<Navigate to="/" />} />
-            </Route>
+            <Route path ="/nailpolishes" element ={<NailPolishes/>}/>
+            <Route path = "/create" element= {<Create/>}/>
+            <Route path="/random" element ={<Shuffle/>}/>
+            <Route index path="*" element={<Navigate to="/nailpolishes" />} />
           </Routes>
         </BrowserRouter>
       </TokenProvider>
     </UserProvider>
   );
-}
-
-function ProtectedRouteGuard({ children }) {
-  const { token } = useContext(TokenContext);
-
-  if (!token) {
-    return <Navigate to="/login" />;
-  }
-
-  return <>{children}</>;
 }
