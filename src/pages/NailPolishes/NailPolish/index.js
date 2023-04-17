f07fa6eca2627e68a0bbc471626d7e4b8c007b81
Isabela <isabela_cmoreira@hotmail.com>
2023-04-17 15:41:35 -0300

    feat: created the nail polish card

---
+++ src/pages/NailPolishes/NailPolish/index.js
@@ -0,0 +1,17 @@
+import { Brand, Container, Shade, Title } from "./style";
+import React from "react";
+import dump from "../../../assets/dump.svg";
+
+
+function NailPolish({id,name,brand,shade,handleDeleteNailPolish}){
+    return(
+        <Container>
+            <Title>{name}</Title>
+            <Brand>{brand}</Brand>
+            <Shade>{shade}</Shade>
+            <img src = {dump} alt ="Trash icon" onClick={() => handleDeleteNailPolish(id)}/>
+        </Container>
+    )
+}
+
+export default NailPolish;
